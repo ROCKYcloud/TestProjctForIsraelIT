6414e3ce0b77d04c15c161ba8cb984c357cdca19
Viktor Rykaliuk <viktorrykalyuk@gmail.com>
2020-10-06 16:16:48 +0300

    fix auto login

--- Testprojectforwork/app/src/main/java/com/example/testprojectforwork/view/SplashFragment.kt
+++ Testprojectforwork/app/src/main/java/com/example/testprojectforwork/view/SplashFragment.kt
@@ -8,12 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.testprojectforwork.PreferenceHelper
 import com.example.testprojectforwork.R
+import com.example.testprojectforwork.PreferenceHelper.password
 
 @Suppress("UNREACHABLE_CODE")
 class SplashFragment : Fragment() {
     private val time:Long = 3000
+    private val str = "User_data"
     private lateinit var preferences: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,15 +27,14 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        preferences = view.context.getSharedPreferences("keystore", 0)
+        preferences = PreferenceHelper.customPreference(view.context, str)
         openSignInFragment()
     }
 
     private fun openSignInFragment(){
         Handler().postDelayed({
-            val alias = "a"
-            val password = preferences.getString(alias, null)
-            if (password != null) {
+            val password = preferences.password.toString()
+            if (password.isNullOrEmpty()) {
                 findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
             }else{
                 findNavController().navigate(R.id.action_splashFragment_to_contentFragment)
